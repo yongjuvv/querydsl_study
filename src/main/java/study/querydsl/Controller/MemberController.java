b07485f180138b6442dfeb4695f3b9f66bdba15f
o01071767279@nate.com <o01071767279@nate.com>
2024-05-03 03:05:36 +0900

    controller 코드 추가

--- src/main/java/study/querydsl/Controller/MemberController.java
+++ src/main/java/study/querydsl/Controller/MemberController.java
@@ -1,12 +1,15 @@
 package study.querydsl.Controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.repository.MemberJpaRepositoryV3;
+import study.querydsl.repository.MemberRepository;
 
 import java.util.List;
 
@@ -15,9 +18,15 @@ import java.util.List;
 public class MemberController {
 
     private final MemberJpaRepositoryV3 repository;
+    private final MemberRepository memberRepository;
 
-    @GetMapping("v1/members")
+    @GetMapping("/v1/members")
     public List<MemberTeamDto> searchMemberV1(@ModelAttribute MemberSearchCondition condition) {
         return repository.searchByBuilder(condition);
+    }
+
+    @GetMapping("/v2/members")
+    public Page<MemberTeamDto> searchMemberV2(@ModelAttribute MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageSimple(condition, pageable);
     }
 }
